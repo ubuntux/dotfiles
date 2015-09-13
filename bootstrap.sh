8764d10da8f7416bd4d784eb56b1bf4f205c9e65
Pongstr <twiz.tickler@gmail.com>
2015-09-13 22:34:26 +0800

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,171 +1,58 @@
-#!/usr/bin/env bash
+set -e
+set -u
 
-echo ""
-echo "      ___       ___          ___          ___                   ___      "
-echo "     /  /\     /__/\        /  /\        /  /\         ___     /  /\     "
-echo "    /  /::\    \  \:\      /  /:/_      /  /:/_       /  /\   /  /::\    "
-echo "   /  /:/\:\    \  \:\    /  /:/ /\    /  /:/ /\     /  /:/  /  /:/\:\   "
-echo "  /  /:/~/:/_____\__\:\  /  /:/_/::\  /  /:/ /::\   /  /:/  /  /:/~/:/   "
-echo " /__/:/ /://__/::::::::\/__/:/__\/\:\/__/:/ /:/\:\ /  /::\ /__/:/ /:/___ "
-echo " \  \:\/:/ \  \:\~~\~~\/\  \:\ /~~/:/\  \:\/:/~/://__/:/\:\\  \:\/:::::/ "
-echo "  \  \::/   \  \:\  ~~~  \  \:\  /:/  \  \::/ /:/ \__\/  \:\\  \::/~~~~  "
-echo "   \  \:\    \  \:\       \  \:\/:/    \__\/ /:/       \  \:\\  \:\      "
-echo "    \  \:\    \  \:\       \  \::/       /__/:/         \__\/ \  \:\     "
-echo "     \__\/     \__\/        \__\/        \__\/                 \__\/     "
-echo ""
-echo "        ..........................................................       "
-echo "        . Dotfiles 0.2.0 (Pongstr) for setting up OSX Workspace  .       "
-echo "        .      https://github.com/pongstr/dotfiles.git           .       "
-echo "        ..........................................................       "
-echo ""
+set +e
+DOT_VERSION="0.1.15"
+OSX_VERSION=$(sw_vers -productVersion | awk -F "." '{print $2}')
 
-# To run this, you must download & install the latest Xcode and Commandline Tools
-# https://developer.apple.com/xcode/
-# https://developer.apple.com/downloads/
+# show the banner and wait for a response
+printf "%s" $'\e[1;32m
+       ___       ___          ___          ___                   ___
+      /  /\     /__/\        /  /\        /  /\         ___     /  /\
+     /  /::\    \  \:\      /  /:/_      /  /:/_       /  /\   /  /::\
+    /  /:/\:\    \  \:\    /  /:/ /\    /  /:/ /\     /  /:/  /  /:/\:\
+   /  /:/~/:/_____\__\:\  /  /:/_/::\  /  /:/ /::\   /  /:/  /  /:/~/:/
+  /__/:/ /://__/::::::::\/__/:/__\/\:\/__/:/ /:/\:\ /  /::\ /__/:/ /:/___
+  \  \:\/:/ \  \:\~~\~~\/\  \:\ /~~/:/\  \:\/:/~/://__/:/\:\\  \:\/:::::/
+   \  \::/   \  \:\  ~~~  \  \:\  /:/  \  \::/ /:/ \__\/  \:\\  \::/~~~~
+    \  \:\    \  \:\       \  \:\/:/    \__\/ /:/       \  \:\\  \:\
+     \  \:\    \  \:\       \  \::/       /__/:/         \__\/ \  \:\
+      \__\/     \__\/        \__\/        \__\/                 \__\/\e[1;31m
+  Version 0.2.0
+\e[0m
+  TODO: Add startup message here...
+\e[0;1m
+  Ready to get started? Hit a key with your favorite finger.\e[0m'
+read -n 1 -s
 
-echo ""
-echo "  To run this, you must download & install the latest Xcode and Commandline Tools"
-echo "    > https://developer.apple.com/xcode/"
-echo "    > https://developer.apple.com/downloads/"
-xcode-select --install
 
-# Function to check if a package exists
-check () { type -t "${@}" > /dev/null 2>&1; }
+echo "
 
-# Function to install Homebrew Formulas:
-install_formula () {
-  echo ""
-  echo "Installing Homebrew Packages:"
+--> TODO: Add password prompt message here..."
+sudo -p "    Password for sudo: " echo "    Sweet, thanks. I'll see you in Vegas, sucker."
 
-  # Excluding this for now, not really a
-  # fan of installing Java in my machine
-  # echo ""
-  # echo "  ➜ Android SDK"
-  # brew install android-sdk
 
-  echo ""
-  echo "  ➜ libyaml"
-  brew install libyaml
+install_dotfiles () {
+  echo "
 
-  echo ""
-  echo "  ➜ Install GNU Scientific Library for rb-gsl"
-  brew install gsl
-
-  echo ""
-  echo "  ➜ openssl"
-  brew install openssl
-
-  echo ""
-  echo "  ➜ git"
-  brew install git
-
-  echo ""
-  echo "  ➜ python"
-  brew install python
-
-  echo ""
-  echo "  ➜ node"
-  brew install node
-
-  echo ""
-  echo "  ➜ mongodb"
-  brew install mongo
-  mkdir $HOME/.mongodb-data
-
-  echo ""
-  echo "  ➜ zsh"
-  brew install zsh
-
-  echo ""
-  echo "  ➜ vim (overriding system vim)"
-  brew install vim --override-system-vi
-
-  # Cleanup
-  echo ""
-  echo "Cleaning up Homebrew installation..."
-  brew cleanup
-
-  cp -R $HOME/.dotfiles/bin/shell/.bashrc $HOME/.bashrc
-  cp -R $HOME/.dotfiles/bin/shell/.bash_alias $HOME/.bash_alias
-  cp -R $HOME/.dotfiles/bin/shell/.bash_profile $HOME/.bash_profile
-
-  echo "Installing Caskroom, Caskroom versions, Caskroom Fonts and Brew Services"
-  brew install caskroom/cask/brew-cask
-  brew tap homebrew/services
-  brew tap caskroom/versions
-  brew tap caskroom/fonts
-
-  # Make /Applications the default location of apps
-  export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-
-  echo ""
-  echo "Installing monospace fonts... "
-  brew cask install font-droid-sans-mono
-  brew cask install font-ubuntu
-  brew cleanup
+--> Download and Install Dotfiles"
+  sudo curl -# -L -O https://github.com/pongstr/dotfiles/archive/${DOT_VERSION}.tar.gz
+  sudo tar -xzf ${DOT_VERSION}.tar.gz --strip=1 -C /opt/dotfiles && rm -rf ${DOT_VERSION}.tar.gz
 }
 
+echo "
 
-# Install Hushlogin
-echo ""
-echo "Install hushlogin"
-echo "  - Disable the system copyright notice, the date and time of the last login."
-echo "    more info at @mathiasbynens/dotfiles http://goo.gl/wZBM80"
-echo ""
-cp -f "$HOME/.dotfiles/.hushlogin" $HOME/.hushlogin
+--> Making sure /opt/dotfiles exists and belongs to you."
 
+sudo mkdir -p /opt/dotfiles
+sudo chown $USER:staff /opt/dotfiles
 
-# Install Homebrew
-# ---------------------------------------------------------------------------
-echo ""
-echo "Checking if Homebrew is installed..."
-
-if check brew; then
-  echo "Awesome! Homebrew is installed! Now updating..."
+if [ ! -d "/opt/" ]; then
   echo ""
-  brew upgrade
-  brew update --all
+  sudo mkdir -p /opt/dotfiles
+  install_dotfiles
+  sudo chown ${USER}:staff /opt/dotfiles
+else
+  install_dotfiles
+  sudo chown ${USER}:staff /opt/dotfiles
 fi
-
-if ! check brew; then
-  echo "Download and install homebrew"
-  echo ""
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-  # Run Brew doctor before anything else
-  brew doctor
-fi
-
-# Install Homebrew Formulas
-while true; do
-  read -p "Would you like to install Homebrew formulas? [y/n] " answer
-  echo ""
-  case $answer in
-    [y/Y]* ) install_formula; break;;
-    [n/N]* ) break;;
-    * ) echo "Please answer Y or N.";;
-  esac
-done
-
-
-# Install RVM
-# ---------------------------------------------------------------------------
-
-echo ""
-echo "Installing RVM and latest version of Ruby"
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-
-echo ""
-echo "  >   Update OSX SSL Certificates"
-rvm osx-ssl-certs update all
-
-echo "  >   Auto update SSL Certificates"
-rvm osx-ssl-certs cron install
-
-sleep 1
-rvm reload
-
-# Restart Terminal for RVM to take effect
-echo ""
-echo "bootstrapping complete! quitting terminal..."
-killall Terminal
